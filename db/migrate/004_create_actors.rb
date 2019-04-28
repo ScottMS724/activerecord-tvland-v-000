c468594c7444f993134e1bc0a0bef946b7c375a2
Scott Silverman <scottms724@gmail.com>
2019-04-28 19:34:49 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -4,6 +4,8 @@ class CreateActors < ActiveRecord::Migration[4.2]
     create_table :actors do |t|
       t.string :first_name 
       t.string :last_name 
+      t.integer :show_id 
+      t.integer :character_id 
     end 
   end 
   
